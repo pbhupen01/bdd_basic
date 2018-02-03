f7a486e26f4f2959ec9ecdc5be79fa59e60dda36
pbhupen01 <pandeybhupen01@gmail.com>
2018-02-03 17:14:13 +0530

    First checkin

---
+++ src/main/java/com/practice/bdd/dto/Cat.java
@@ -0,0 +1,18 @@
+package com.practice.bdd.dto;
+
+public class Cat {
+
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
